26a70489c26f53bbb757fa295f553d9df43160aa
Anna Schmidt <marshall1697@gmail.com>
2022-12-15 11:45:09 -0700

    fixed navbar and about

--- src/components/About.js
+++ src/components/About.js
@@ -11,13 +11,13 @@ const About = () => {
         A bit about me. . . 
         </h3>
         <p>
-        I'm a software developer with experience building full stack applications with React.js and Ruby on Rails. I completed the software engineering bootcamp at Flatiron School in December 2022. I love that learning is a continuous and integral part of the programming. I thoroughly enjoy growing my programming skills and am currently learning to build with Node.js and Python. 
+        I'm a full stack software engineer passionate about developing creative solutions that can improve lives. I completed the software engineering bootcamp at Flatiron School in December 2022. I love that learning is a continuous and integral part of the programming and thoroughly enjoy growing my programming skills. I am currently learning to build with Node.js and Python. 
         </p>
         <p>
-        My unconventional path to software development started while working as a critical-care nurse. I love to help others, and have a lot of hands-on experience doing so. Before enrolling in the Flatiron coding bootcamp, I completed multiple online courses (mostly through Udemy), reading books on html, css, and javascript and building projects. I learned my side-interest was something I wanted to pursue full-time. I'm in awe of the infinite possibilities this career field allows in terms of collaborating and creating programs that have the potential to reach and help numerous people. 
+        My unconventional path to software development started while working as an emergency room and critical-care nurse. I love to help others, and have a lot of hands-on experience doing so. Before enrolling in the Flatiron coding bootcamp, I completed multiple online courses (mostly through Udemy), reading books on html, css, and javascript and building projects. I learned my side-interest was something I wanted to pursue full-time. I'm in awe of the infinite possibilities this career field allows in terms of collaborating and creating programs that have the potential to reach and help numerous people. 
         </p>
         <p>
-        Aside from coding, I LOVE being outside! I'm fortunate to call Colorado my home. High alpine lakes, technical ridgelines, and running many miles on trails overgrown with wildflowers are a few of my favorite things.
+        Aside from coding, I LOVE being outside and staying active! I'm fortunate to call Colorado my home. High alpine lakes, technical ridgelines, and running many miles on trails overgrown with wildflowers are a few of my favorite things.
         </p>
       </div>
       <div className="half-page-image"></div>
