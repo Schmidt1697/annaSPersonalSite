ef0dfc3366906282963a367cd95ef0ca8a359d90
Anna Schmidt <marshall1697@gmail.com>
2023-02-06 10:12:51 -0700

    updated About.js and css/added medium

--- src/components/About.js
+++ src/components/About.js
@@ -11,16 +11,15 @@ const About = () => {
         A bit about me. . . 
         </h3>
         <p>
-        I'm a full stack software engineer passionate about developing creative solutions that can improve lives. I completed the software engineering bootcamp at Flatiron School in December 2022. I love that learning is a continuous and integral part of programming and thoroughly enjoy growing my programming skills. I am currently learning to build with Node.js and Python. 
+        I'm a full stack software engineer passionate about developing creative solutions to improve lives. I completed the software engineering bootcamp at Flatiron School in December 2022. I love that learning is a continuous and integral part of programming and thoroughly enjoy growing my programming skills. I am currently learning Node.js, Express.js and MongoDB. 
         </p>
         <p>
-        My unconventional path to software development started while working as an emergency room and critical-care nurse. I love to help others, and have a lot of hands-on experience doing so. Before enrolling in the Flatiron coding bootcamp, I completed multiple online courses (mostly through Udemy), read books on html, css, and javascript and built projects. I learned my side-interest was something I wanted to pursue full-time. I'm in awe of the infinite possibilities this career field allows in terms of collaborating and creating programs that have the potential to reach and help numerous people. 
+        My unconventional path to software development started while working as an emergency room and critical-care nurse. I love to help others, and have a lot of hands-on experience doing so. Before enrolling in the Flatiron School coding bootcamp, I completed multiple online courses/projects, and read books on HTML, CSS, and JavaScript. I'm in awe of the infinite possibilities this career field allows in terms of collaborating and creating programs that have the potential to reach and help numerous people.
         </p>
         <p>
-        Aside from coding, I LOVE being outside and staying active! I'm fortunate to call Colorado my home. High alpine lakes, technical ridgelines, and running many miles on trails overgrown with wildflowers are a few of my favorite things.
+        Aside from coding, I LOVE exploring outside! I'm fortunate to call Colorado my home. High alpine lakes, technical ridgelines, and running many miles on trails overgrown with wildflowers are a few of my favorite things.
         </p>
       </div>
-      <div className="half-page-image"></div>
 
     </div>
   )
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,25 +1,44 @@
-import { useState} from 'react'
-import Contact from './Contact'
+import { useState } from 'react';
+import Contact from './Contact';
 
 const Footer = () => {
-  const [openModal, setOpenModal] = useState(false)
+	const [openModal, setOpenModal] = useState(false);
 
-    
-  //open/close contact modal
-  const handleOpen = () => setOpenModal(true);
-  const handleClose = () => setOpenModal(false);
+	//open/close contact modal
+	const handleOpen = () => setOpenModal(true);
+	const handleClose = () => setOpenModal(false);
 
-  return (
-    <div className='footer-container'>
-       <p>Interested in collaborating?</p>
-       <button className='contact-btn' onClick={handleOpen}>Contact Me</button>
-             <div className="social-media">
-              <a href="https://www.linkedin.com/in/annaschmidt1697/" rel="noopener noreferrer" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
-              <a href="https://github.com/Schmidt1697" target ="_blank" rel="noopener noreferrer"><i className="fa-brands fa-github"></i></a>
-          </div>
-      <Contact openModal={openModal} handleClose={handleClose} />
-    </div>
-  )
-}
+	return (
+		<div className="footer-container">
+			<p>Interested in collaborating?</p>
+			<button className="contact-btn" onClick={handleOpen}>
+				Contact Me
+			</button>
+			<div className="social-media">
+      <a
+					href="https://github.com/Schmidt1697"
+					target="_blank"
+					rel="noopener noreferrer">
+					<i className="fa-brands fa-github"></i>
+				</a>
+        
+				<a
+					href="https://www.linkedin.com/in/annaschmidt1697/"
+					rel="noopener noreferrer"
+					target="_blank">
+					<i className="fa-brands fa-linkedin"></i>
+				</a>
+			
+				<a
+					href="https://medium.com/@marshall1697"
+					target="_blank"
+					rel="noopener noreferrer">
+					<i class="fa-brands fa-medium"></i>
+				</a>
+			</div>
+			<Contact openModal={openModal} handleClose={handleClose} />
+		</div>
+	);
+};
 
-export default Footer
+export default Footer;
